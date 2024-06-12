2955bc91e1e383e1b2dec621a5c88f231bf4cef9
Simone Riggi <simone.riggi@gmail.com>
2024-06-12 11:04:30 +0200

    Add multi gpu inference in MPI run

--- scripts/run.py
+++ scripts/run.py
@@ -265,8 +265,6 @@ def main():
 	if not devices:
 		logger.error("Empty list of devices for inference, at least one must be given!")
 		return 1
-	print("devices")
-	print(devices)
 		
 	#==============================
 	#==   DEFINE PRE-PROCESSOR
